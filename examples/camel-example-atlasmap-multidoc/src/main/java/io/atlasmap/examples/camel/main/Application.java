433e4f0ecaa5c0fb05b0bc7c6c9a46bae693e4e3
tplevko <tplevko@redhat.com>
2020-10-02 13:15:58 +0200

    state with ENTESB-12361 and generated mapping

--- examples/camel-example-atlasmap-multidoc/src/main/java/io/atlasmap/examples/camel/main/Application.java
+++ examples/camel-example-atlasmap-multidoc/src/main/java/io/atlasmap/examples/camel/main/Application.java
@@ -10,15 +10,15 @@ public class Application extends RouteBuilder {
         from("timer:main?period=5000")
             .to("direct:order-producer")
             .to("direct:contact-producer")
-            .to("atlas:atlasmap-mapping.adm")
+            .to("atlas:atlasmapping-UI.0_new.json")
             .to("direct:outcome-consumer");
 
         from("direct:order-producer")
-            .setProperty("order-schema", simple("resource:classpath:data/order.json"))
+            .setProperty("order-schema-2426fd12-f113-4e55-a930-0d5bac62d09f", simple("resource:classpath:data/order.json"))
             .log("-->; Order: [${exchangeProperty.order-schema}]");
         
         from("direct:contact-producer")
-            .setProperty("contact-schema", simple("resource:classpath:data/contact.xml"))
+            .setProperty("contact-schema-215b0c4d-8cc4-4d9c-88b9-68011ac40b75", simple("resource:classpath:data/contact.xml"))
             .log("-->; Contact: [${exchangeProperty.contact-schema}]");
         
         from("direct:outcome-consumer")
